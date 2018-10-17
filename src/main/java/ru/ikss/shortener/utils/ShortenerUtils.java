f105b4f3caef3a914fc1fbbed792537c9776df18
s.kuznetsov <s.kuznetsov@dreamkas.ru>
2018-10-17 10:32:57 +0300

    Add test for zero index and got rid of unnesecary arrayCopy in StringBuilder

--- src/main/java/ru/ikss/shortener/utils/ShortenerUtils.java
+++ src/main/java/ru/ikss/shortener/utils/ShortenerUtils.java
@@ -6,16 +6,20 @@ public class ShortenerUtils {
 
     public static String encode(long num) {
         StringBuilder str = new StringBuilder();
-        while (num > 0) {
-            str.insert(0, ALPHABET.charAt((int) (num % BASE)));
-            num = num / BASE;
+        if (num == 0) {
+            str.append(ALPHABET.charAt(0));
+        } else {
+            while (num > 0) {
+                str.append(ALPHABET.charAt((int) (num % BASE)));
+                num = num / BASE;
+            }
         }
         return str.toString();
     }
 
     public static long decode(String str) {
         long num = 0;
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = str.length() - 1; i >= 0; i--) {
             num = num * BASE + ALPHABET.indexOf(str.charAt(i));
         }
         return num;
--- src/test/java/ru/ikss/shortener/utils/ShortenerUtilsTest.java
+++ src/test/java/ru/ikss/shortener/utils/ShortenerUtilsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Tests for shorten algorithm")
 public class ShortenerUtilsTest {
@@ -13,6 +14,22 @@ public class ShortenerUtilsTest {
     @DisplayName("Test inverse")
     public void inverseTest() {
         long id = RandomUtils.nextLong(1, Long.MAX_VALUE);
+        String shorten = ShortenerUtils.encode(id);
+        assertEquals(id, ShortenerUtils.decode(shorten), "Test that encoding and decoding results same id");
+    }
+
+    @Test
+    @DisplayName("Test zero index")
+    public void zeroIndexTest() {
+        long id = 0;
+        String shorten = ShortenerUtils.encode(id);
+        assertTrue(shorten.length() > 0, "Test that zero id has short representation");
+    }
+
+    @Test
+    @DisplayName("Test zero index equality")
+    public void zeroIndexEqualityTest() {
+        long id = 0;
         String shorten = ShortenerUtils.encode(id);
         assertEquals(id, ShortenerUtils.decode(shorten), "Test that encoding and decoding results same id");
     }
